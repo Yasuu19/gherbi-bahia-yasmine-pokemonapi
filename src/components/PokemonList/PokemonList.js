146d2de0c27ec004f5216e0807929d814b7a937a
Yasuu19 <yasminegherbibahia@gmail.com>
2023-05-17 11:10:45 +0200

    feat: exo 1, 2 et 3

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,13 +1,35 @@
-import React  from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function PokemonList({ searchTerm = "" }) {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  async function getData() {
+    //On récupère les données de l'API
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100)");
+    // On stocke les données dans la liste pokemons
+    setPokemons(res.data.results);
+  }
+
+  //A chaque changement on appelle la fonction getData qui appelle l'API
+  useEffect(() => {
+    getData();
+  }
+    , []);
 
 
   return (
     <ul>
-    <li>test</li>
-    <li>test2</li>
-    </ul>
+      //Affiche la liste des pokemons et filtre
+      {pokemons.filter((pokemon) => {
+        return pokemon.name.includes(searchTerm)
+      }).map((pokemon) => {
+        // Ajout d'une clé pouro chaque pokemon
+        return <li key={pokemon.name}>{pokemon.name}</li>
+      })}
+    </ul >
   );
 }
 
